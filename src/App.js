f3b451c8dc67fd162dc231d8144d2b6769276c46
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2020-06-03 21:41:16 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 function App() {
+  // Reactはreturnするタグをひとつにしないといけない
+  // そのために不要なdivが嫌な場合には、React.Fragmentを使う
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <React.Fragment>
+      <label htmlFor='bar'>bar</label>
+      <input
+        type='text'
+        onChange={() => {
+          console.log('clicked!');
+        }}
+      />
+    </React.Fragment>
   );
 }
 
