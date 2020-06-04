41bec95b3f609748cdfee81e7bc58dbda5f355df
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2020-06-04 20:10:55 +0900

    class / function component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  // Reactはreturnするタグをひとつにしないといけない
-  // そのために不要なdivが嫌な場合には、React.Fragmentを使う
+// 関数コンポーネント
+// 講義と表記が違うのは、講義ではクラスコンポーネントになっているから
+// function App() {
+//   // Reactはreturnするタグをひとつにしないといけない
+//   // そのために不要なdivが嫌な場合には、React.Fragmentを使う
+//   return (
+//     <React.Fragment>
+//       <label htmlFor='bar'>bar</label>
+//       <input
+//         type='text'
+//         onChange={() => {
+//           console.log('clicked!');
+//         }}
+//       />
+//     </React.Fragment>
+//   );
+// }
+
+// クラスコンポーネント
+// class App extends Component {
+//   render() {
+//     return <div>Hi form Class component</div>;
+//   }
+// }
+
+// 関数コンポーネント
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor='bar'>bar</label>
-      <input
-        type='text'
-        onChange={() => {
-          console.log('clicked!');
-        }}
-      />
-    </React.Fragment>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   );
-}
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
