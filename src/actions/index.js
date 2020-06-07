ebe594030ff5268684302c381bfff943177bef83
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2020-06-07 11:06:11 +0900

    implement action & action creator

---
+++ src/actions/index.js
@@ -0,0 +1,21 @@
+// ActionはJSのオブジェクト
+// typeというキーと、その値を持つ
+// typeの値はユニークでなければならない
+// {
+//   type: 'INCREMENT',
+// }
+// Actionを返す関数をトクションクリエータと呼ぶ
+
+// アクションクリエータ
+// オブジェクトを返すときは外側に小カッコが必要（だと思う）
+export const increment = () => ({
+  type: 'INCREMENT',
+});
+
+export const decrement = () => ({
+  type: 'DECREMENT',
+});
+
+// 他のところでも使うのでexportしておく
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
